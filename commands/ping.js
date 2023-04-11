d38b3c88a444a446230b1ea8d2d4e71e10cd4e76
woolleyj20 <59579029+woolleyj20@users.noreply.github.com>
2023-04-10 21:23:55 -0700

    Base embed layout
    Data collected and the base layout for information created

--- commands/ping.js
+++ commands/ping.js
@@ -1,12 +1,11 @@
 const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+const { cards } = require('../hero-cards.json');
 
-const cardMap = new Map();
-cardMap.set('correct', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTvrrU9hRt6mtgUqYoYpm2jv5NUAI5J6OEEJFkhe6rWk_Wo6XzEFSeHmeCcFg1nGAMTIl4&usqp=CAU');
 
 module.exports = {
 	data: new SlashCommandBuilder()
-		.setName('ping')
-		.setDescription('Replies with Pong!')
+		.setName('card')
+		.setDescription('Find a card/equipment by name')
 		.addStringOption(option =>
 			option.setName('card_name')
 				.setDescription('The card to fetch')
@@ -19,14 +18,38 @@ module.exports = {
 };
 
 function buildResponse(name) {
-	if (cardMap.has(name)) {
-		const url = cardMap.get(name);
+	const card = cards.find(element => element.name == name);
+	if (typeof card !== 'undefined') {
+		const cardName = card.name;
+		const baseDescription = descriptionReturn(card.versions.base.description);
+		const blueDescription = descriptionReturn(card.versions.blue.description);
+		const yellowDescription = descriptionReturn(card.versions.yellow.description);
+		const corruptedDescription = descriptionReturn(card.versions.corrupted.description);
+
+		const url = card.url ?? 'https://st.depositphotos.com/1106005/3146/i/450/depositphotos_31468817-stock-photo-coming-soon-sign.jpg';
 		return new EmbedBuilder()
-			.setTitle(name)
+			.setTitle(cardName)
+			.addFields(
+				{ name: 'Base Information', value: ' ' },
+			)
+			.addFields(
+				{ name: 'Description', value: baseDescription, inline: true },
+				{ name: 'Target', value: card.versions.base.target, inline: true },
+				{ name: 'Cost', value: String(card.versions.base.cost), inline: true },
+			)
 			.setImage(url);
 	}
 	else {
 		return new EmbedBuilder()
-			.setDescription('A card by this name does not exist');
+			.setDescription('A card by this name could not be found :slight_frown:');
+	}
+}
+
+function descriptionReturn(description) {
+	if (description == '') {
+		return 'Description missing, coming soon!';
+	}
+	else {
+		return description;
 	}
 }
