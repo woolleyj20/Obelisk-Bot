1725cb581a87e097f4e613f55b20ec240b0ce262
woolleyj20 <59579029+woolleyj20@users.noreply.github.com>
2023-04-11 14:43:53 -0700

    Full information embed
    All information needed is in the embed, with some edits to formatting in the cards data

--- commands/ping.js
+++ commands/ping.js
@@ -9,36 +9,57 @@ module.exports = {
 		.addStringOption(option =>
 			option.setName('card_name')
 				.setDescription('The card to fetch')
-				.setRequired(true)),
+				.setRequired(true))
+		.addStringOption(option =>
+			option.setName('version')
+				.setDescription('The version of the card')
+				.setRequired(true)
+				.addChoices(
+					{ name: 'Base', value: 'base' },
+					{ name: 'Blue', value: 'blue' },
+					{ name: 'Yellow', value: 'yellow' },
+					{ name: 'Corrupted', value: 'corrupted' },
+				)),
 	async execute(interaction) {
-		const exampleEmbed = buildResponse(interaction.options.getString('card_name'));
+		const exampleEmbed = buildResponse(interaction.options.getString('card_name'), interaction.options.getString('version'));
 
 		await interaction.reply({ embeds: [exampleEmbed] });
 	},
 };
 
-function buildResponse(name) {
+function buildResponse(name, type) {
 	const card = cards.find(element => element.name == name);
 	if (typeof card !== 'undefined') {
 		const cardName = card.name;
-		const baseDescription = descriptionReturn(card.versions.base.description);
-		const blueDescription = descriptionReturn(card.versions.blue.description);
-		const yellowDescription = descriptionReturn(card.versions.yellow.description);
-		const corruptedDescription = descriptionReturn(card.versions.corrupted.description);
+		let vanish = false;
+		if (typeof card.versions[type].vanish !== 'undefined') {
+			vanish = true;
+		}
 
-		const url = card.url ?? 'https://st.depositphotos.com/1106005/3146/i/450/depositphotos_31468817-stock-photo-coming-soon-sign.jpg';
+		let innate = false;
+		if (typeof card.versions[type].innate !== 'undefined') {
+			innate = true;
+		}
+
+		// const url = card.url ?? 'https://st.depositphotos.com/1106005/3146/i/450/depositphotos_31468817-stock-photo-coming-soon-sign.jpg';
+
 		return new EmbedBuilder()
 			.setTitle(cardName)
 			.addFields(
-				{ name: 'Base Information', value: ' ' },
+				{ name: `${capitalizeFirstLetter(type)} Information`, value: ' ' },
 			)
 			.addFields(
-				{ name: 'Description', value: baseDescription, inline: true },
-				{ name: 'Target', value: card.versions.base.target, inline: true },
-				{ name: 'Cost', value: String(card.versions.base.cost), inline: true },
+				{ name: 'Description', value: descriptionReturn(card['versions'][type]['description']), inline: true },
+				{ name: 'Target', value: card.versions[type].target, inline: true },
+				{ name: 'Cost', value: String(card.versions[type].cost), inline: true },
 			)
-			.setImage(url);
-	}
+			.addFields(
+				{ name: 'Card Types', value: card.cardTypes.join('\n'), inline: true },
+				{ name: 'Vanish', value: capitalizeFirstLetter(String(vanish)), inline: true },
+				{ name: 'Innate', value: capitalizeFirstLetter(String(innate)), inline: true },
+			);
+
+		}
 	else {
 		return new EmbedBuilder()
 			.setDescription('A card by this name could not be found :slight_frown:');
@@ -52,4 +73,8 @@ function descriptionReturn(description) {
 	else {
 		return description;
 	}
+}
+
+function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
 }
